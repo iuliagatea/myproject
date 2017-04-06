f419b4f7e57d341e7e3fbf1f946096be0c14ab04
Iulia Gatea <juliagatea@yahoo.com>
2017-04-06 08:56:06 +0000

    updated navbar and user registration and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+   devise_for :users, :controllers => { registrations: 'registrations' }
   get 'pages/index'
 
   #get 'hello', to: 'pages#contact'
