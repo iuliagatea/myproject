3478db50257a64d0d586222627907abafce11bcb
Iulia Gatea <juliagatea@yahoo.com>
2017-04-11 07:36:34 +0000

    scaffold crud articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-   devise_for :users, :controllers => { registrations: 'registrations' }
-  get 'pages/index'
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :articles
+  #get 'pages/index'
 
   #get 'hello', to: 'pages#contact'
 
